c80eab78cfe98fbeb271432c375abadc6e3caef8
alvruiza <alvaro@xocolatery.cl>
2022-07-08 16:36:41 -0400

    creado modal con bootstrap y código JS que capta las tareas y env{ía array al local storage

--- CRUD.js
+++ CRUD.js
@@ -1,11 +1,61 @@
+const taskForm = document.getElementById("addbtn");
+const form = document.getElementById("taskform");
+const modal = document.getElementById("addtaskmodal");
 
 
 let tareas = []
 
-const addTask = (task) => {
+const getValues = (taskForm) => {    
+    let newTask = document.querySelector("#task-text").value;
+    let newName = document.querySelector("#recipient-name").value;
+    let task = {
+        Nombre: newName,
+        Tarea: newTask,
+    }
+    tareas.push(task)      
+    console.log(tareas)
+    form.reset()          
+}
+
+const saveTasks = () => {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+taskForm.addEventListener("click", (save) => {
+    getValues() 
+    $("#addtaskmodal").modal("hide");
+    saveTasks()
+})
+
+document.addEventListener("DOMContentLoaded", (printTasks) => {
+
+})
+
+
+
+
+
+
+/* const addTask = (tarea, aName) => {
   let task = {
-    task: task,
-    aName: aName
+    Tarea: tarea,
+    Nombre: aName
   }  
     tareas.push(task);
-}
+}*/
+
+/* taskForm.addEventListener("#addbtn", (newTaskBtn) => {
+    newTaskBtn.preventdefault();    
+    let newTask = document.querySelector("#task-text").value;
+    let newName = document.querySelector("#recipient-name").value;   
+    
+    const saveTask = {
+        Tarea: `${newTask}`,
+        Nombre: `${newName}`
+    }
+    
+    taskForm.reset()
+
+    console.log(saveTask)  
+})
+*/
